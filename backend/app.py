a9ba7eb4431f3745b081e0b18e674f4a81921113
Dimitrios Gkegkas <dimitrisgegas01@gmail.com>
2022-07-07 21:26:07 +0300

    Fix

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from dalle_model import DalleModel
 import argparse
 import base64
 import os
@@ -7,46 +8,38 @@ import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-
-from flax.jax_utils import replicate
-from flax.training.common_utils import shard_prng_key
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-import os
-
-cloudinary.config( 
-  cloud_name = os.environ['CLOUD_NAME'], 
-  api_key = os.environ['API_KEY'], 
-  api_secret = os.environ['API_SECRET']
-)
-
-# type used for computation - use bfloat16 on TPU's
-dtype = jnp.bfloat16 if jax.local_device_count() == 8 else jnp.float32
-
-# TODO: fix issue with bfloat16
-dtype = jnp.float32
-
-import wandb
-
 from consts import DEFAULT_IMG_OUTPUT_DIR
 from utils import parse_arg_boolean, parse_arg_dalle_version
 from consts import ModelSize
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+    cloud_name=os.environ['CLOUD_NAME'],
+    api_key=os.environ['API_KEY'],
+    api_secret=os.environ['API_SECRET']
+)
 
 app = Flask(__name__)
 CORS(app)
 print("--> Starting DALL-E Server. This might take up to two minutes.")
 
-from dalle_model import DalleModel
 dalle_model = None
 
-parser = argparse.ArgumentParser(description = "A DALL-E app to turn your textual prompts into visionary delights")
-parser.add_argument("--port", type=int, default=8000, help = "backend port")
-parser.add_argument("--model_version", type = parse_arg_dalle_version, default = ModelSize.MINI, help = "Mini, Mega, or Mega_full")
-parser.add_argument("--save_to_disk", type = parse_arg_boolean, default = False, help = "Should save generated images to disk")
-parser.add_argument("--img_format", type = str.lower, default = "JPEG", help = "Generated images format", choices=['jpeg', 'png'])
-parser.add_argument("--output_dir", type = str, default = DEFAULT_IMG_OUTPUT_DIR, help = "Customer directory for generated images")
+parser = argparse.ArgumentParser(
+    description="A DALL-E app to turn your textual prompts into visionary delights")
+parser.add_argument("--port", type=int, default=8000, help="backend port")
+parser.add_argument("--model_version", type=parse_arg_dalle_version,
+                    default=ModelSize.MINI, help="Mini, Mega, or Mega_full")
+parser.add_argument("--save_to_disk", type=parse_arg_boolean,
+                    default=False, help="Should save generated images to disk")
+parser.add_argument("--img_format", type=str.lower, default="JPEG",
+                    help="Generated images format", choices=['jpeg', 'png'])
+parser.add_argument("--output_dir", type=str, default=DEFAULT_IMG_OUTPUT_DIR,
+                    help="Customer directory for generated images")
 args = parser.parse_args()
+
 
 @app.route("/dalle", methods=["POST"])
 @cross_origin()
@@ -57,24 +50,26 @@ def generate_images_api():
     generated_imgs = dalle_model.generate_images(text_prompt, num_images)
 
     returned_generated_images = []
-    if args.save_to_disk: 
-        dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H:%M:%S')}_{text_prompt}")
+    if args.save_to_disk:
+        dir_name = os.path.join(
+            args.output_dir, f"{time.strftime('%Y-%m-%d_%H:%M:%S')}_{text_prompt}")
         Path(dir_name).mkdir(parents=True, exist_ok=True)
-    
+
     for idx, img in enumerate(generated_imgs):
-        if args.save_to_disk: 
-          img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
+        if args.save_to_disk:
+            img.save(os.path.join(
+                dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
 
         buffered = BytesIO()
         img.save(buffered, format=args.img_format)
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
         result = cloudinary.uploader.upload("data:image/jpeg;base64,"+img_str)
-        generated_images.append(result["url"])
+        returned_generated_images.append(result["url"])
 
     print(f"Created {num_images} images from text prompt [{text_prompt}]")
-    
+
     response = {'generatedImgs': returned_generated_images,
-    'generatedImgsFormat': args.img_format}
+                'generatedImgsFormat': args.img_format}
     return jsonify(response)
 
 
